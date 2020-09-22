793c5cb39b7b9bf7da92d81b6761f81a4b6f7d98
Marquis Whitley <whitley.marquis1@yahoo.com>
2020-09-22 18:50:54 -0400

    Modified Test documents

--- delete.js
+++ delete.js
@@ -1,5 +1,92 @@
-const prev = document.querySelector('.prev')
-const next = document.querySelector('.next')
+const slideImage = document.querySelectorAll('.slide-image');
+const slidesContainer = document.querySelector('.slides-container');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+const navigationDots = document.querySelector('.navigation-dots');
 
-console.log(next)
-console.log(prev)
+let numberOfImages = slideImage.length;
+let slideWidth = slideImage[0].clientWidth;
+let currentSlide = 0;
+
+console.log(slidesContainer)
+// Set up the slider
+
+function init() {
+    /* 
+    
+    slideImage[0] = 0
+    slideImage[1] = 100%
+    slideImage[2] = 200%
+    ...
+    */
+
+    slideImage.forEach((img, i) => {
+        img.style.left = i * 100 + '%'
+    });
+
+    slideImage[0].classList.add('active');
+
+    createNavigationDots();
+}
+
+init();
+
+// Create navigation dots
+
+function createNavigationDots() {
+    for(let i = 0; i < numberOfImages;i++) {
+        const dot = document.createElement('div');
+        dot.classList.add('single-dot')
+        navigationDots.appendChild(dot)
+    }
+
+    navigationDots.children[0].classList.add('active');
+}
+
+// Next Button
+
+nextBtn.addEventListener('click', () => {
+    if(currentSlide >= numberOfImages - 1){
+        goToSlide(0);
+        return;
+    }
+    currentSlide++;
+    goToSlide(currentSlide);
+});
+
+// Prev Button
+
+prevBtn.addEventListener('click', () => {
+    if(currentSlide <= 0){
+        goToSlide(numberOfImages - 1);
+        return;
+    }
+    currentSlide--;
+    goToSlide(currentSlide);
+});
+
+// Go To Slide
+
+function goToSlide(slideNumber) {
+    slidesContainer.style.transform = 'translateX(-' + slideWidth * slideNumber + 'px)';
+
+    currentSlide = slideNumber;
+
+    setActiveClass();
+}
+
+// Set Active Class
+
+function setActiveClass() {
+    // Set active class for Slide Image
+
+    let currentActive = document.querySelector('.slide-image.active')
+    currentActive.classList.remove('active');
+    slideImage[currentSlide].classList.add('active')
+
+    // set active class for navigation dots
+
+    let currentDot = document.querySelector('.single-dot.active')
+    currentDot.classList.remove('active');
+    navigationDots.children[currentSlide].classList.add('active')
+}
